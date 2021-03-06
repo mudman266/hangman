58441d7796194e61ea9dc8704a9aa3e54a075eee
Josh Williams <josh@jwillcreations.com>
2021-03-06 13:59:48 -0500

    Now detects a winner and displays an appropriate message.

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ class Hangman(qtw.QMainWindow):
             if debugging:
                 print("Alpha input found.")
             if len(guessing) > 1:
+                # Too many letters entered
                 if debugging:
                     print("More than 1 letter input.")
                 qtw.QMessageBox.critical(self, "Too Many Letters", "You have Entered more than 1 letter. Try again.")
@@ -62,6 +63,10 @@ class Hangman(qtw.QMainWindow):
                     print("New letter guessed: {:}".format(guessing))
                     letters_guessed.append(guessing.upper())
                     self.draw_board()
+        else:
+            # Non alpha character found
+            print("Non-Alpha character found.")
+            qtw.QMessageBox.critical(self, "Invalid Character", "Only letters A-Z accepted.")
 
     def draw_board(self):
         # Update the guessed letters on screen
@@ -81,7 +86,7 @@ class Hangman(qtw.QMainWindow):
                 correct_word += " _ "
         self.ui.lblLettersGuessedTitle_2.setText(correct_word)
 
-        # Draw the man for any wrong letters
+        # Draw the man for any wrong guesses
         if letters_guessed[len(letters_guessed) - 1] not in self.puzzle:
             self.wrong_guesses += 1
             if self.wrong_guesses > 5:
@@ -103,6 +108,17 @@ class Hangman(qtw.QMainWindow):
             if self.wrong_guesses == 1:
                 print("Wrong answer 1.")
                 self.ui.btnHead.setVisible(True)
+
+        # Handle winning
+        if " _ " not in correct_word:
+            print("Winner winner chicken dinner!")
+            # Build the goal word to inform the winner!
+            goal_word = ""
+            for let in self.puzzle:
+                goal_word += let
+            qtw.QMessageBox.information(self, "Winner!", "Winner! Congrats! The correct word was: {:}".format(goal_word))
+            exit(0)
+
 
     def game_over(self):
         print("Running Game Over")
